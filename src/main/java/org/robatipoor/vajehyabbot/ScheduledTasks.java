9d3d0134832c93c1cf97efcd1b4131afb64bbb8c
robatioor <mahdi.robatipoor@gmail.com>
2019-02-26 21:38:54 +0330

    add scheduling task

--- src/main/java/org/robatipoor/vajehyabbot/ScheduledTasks.java
+++ src/main/java/org/robatipoor/vajehyabbot/ScheduledTasks.java
@@ -17,10 +17,10 @@ public class ScheduledTasks {
 
     private static final Logger log = LogManager.getLogger(ScheduledTasks.class);
 
-    @Scheduled(fixedRate = 60000 * 30)
+    @Scheduled(fixedRate = 60000 * 1)
     public void sendRequest() throws Exception {
         OkHttpClient client = new OkHttpClient();
-        Request request = new Request.Builder().url("https://yajehyabbot.herokuapp.com/").build();
+        Request request = new Request.Builder().header("User-Agent", "Mozilla/5.0 (X11; Linux i686; rv:64.0) Gecko/20100101 Firefox/64.0").url("https://yajehyabbot.herokuapp.com/").build();
         try (Response response = client.newCall(request).execute()) {
             log.info(response.body().string());
         }
